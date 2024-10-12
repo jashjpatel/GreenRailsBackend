b456c8197870a063a3bbfa94d943cf221d4a8982
Jash <jashpatelj@gmail.com>
2024-10-12 11:29:35 -0700

    this one comma fixes the errors caused by the scan_ruby job lol

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   get "up" => "rails/health#show", as: :rails_health_check
