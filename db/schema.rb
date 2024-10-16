153279d3c057a6d30161be1683871a01bd50d0d3
Jash <jashpatelj@gmail.com>
2024-10-16 00:09:30 -0700

    schema changes for the db

--- db/schema.rb
+++ db/schema.rb
@@ -21,5 +21,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_16_070551) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "jti", null: false
+    t.index ["jti"], name: "index_users_on_jti", unique: true
   end
 end
