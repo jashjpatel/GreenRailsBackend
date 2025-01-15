0ba13ec48964150a1c1fe0bfd6139da2cce4d0e6
Jash <jashpatelj@gmail.com>
2025-01-14 16:39:35 -0800

    fixing the sytanx

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,24 +3,24 @@ lock "~> 3.19.2"
 
 set :application, "GreenRailsBackend"
 set :repo_url, "git@github.com:jashjpatel/GreenRailsBackend.git"
-set :deploy_to, "/home/deploy/aws-rails" 
+set :deploy_to, "/home/deploy/aws-rails"
 
-set :linked_files, %w{config/database.yml config/master.key}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w[config/database.yml config/master.key]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
 
 set :keep_releases, 3
 set :keep_assets, 3
 
 namespace :deploy do
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join("tmp/restart.txt")
     end
   end
 
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  after :publishing, "deploy:restart"
+  after :finishing, "deploy:cleanup"
 end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
